9b17acdc3818f7e8cb03e7ef1918c45c2203a906
nullshii <60722967+nullshii@users.noreply.github.com>
2023-08-28 10:11:21 -0700

    Created simple player jump; circle obstacle

---
+++ Assets/Code/JumpHandler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Code
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class JumpHandler : MonoBehaviour
+    {
+        [SerializeField] private float _jumpVelocity;
+
+        private Rigidbody2D _rigidbody;
+
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            if (Input.anyKeyDown || Input.touchCount > 0)
+            {
+                Jump();
+            }
+        }
+
+        private void Jump()
+        {
+            _rigidbody.velocity = Vector2.up * _jumpVelocity;
+        }
+    }
+}
---
+++ Assets/Code/Rotator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Code
+{
+    public class Rotator : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+        
+        private Transform _transform;
+
+        private void Start()
+        {
+            _transform = transform;
+        }
+
+        private void Update()
+        {
+            _transform.Rotate(0f, 0f, _speed * Time.deltaTime);
+        }
+    }
+}
